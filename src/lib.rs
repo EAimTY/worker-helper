d0d5926118b829985ff97287e5974c4270ec27c2
EAimTY <ea.imty@gmail.com>
2025-11-05 21:19:52 +0800

    re-export `http::Uri`

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub use http::{Method, Request, Response, StatusCode};
+pub use http::{Method, Request, Response, StatusCode, Uri};
 pub use http_body::{Body as BodyTrait, Frame};
 pub use http_body_util::Empty as EmptyBody;
 pub use worker::{Context, Env, Error, event};
