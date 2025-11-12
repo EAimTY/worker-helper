5d9a389108757b9dfaf7d3212e2644d714ea4ced
EAimTY <ea.imty@gmail.com>
2025-11-12 12:35:03 +0700

    rename crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
-pub use http::{Method, Request, Response, StatusCode, Uri};
-pub use http_body::{Body as BodyTrait, Frame};
-pub use http_body_util::Empty as EmptyBody;
-pub use worker::{Context, Env, Error, event};
-
 pub mod body;
 pub mod fetch;
 
 pub use crate::{body::*, fetch::*};
+pub use http;
+pub use http_body;
+pub use http_body_util;
